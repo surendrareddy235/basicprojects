2081a93f9a04b8b1a91554e64b6ef6b71a4f9bf8
surendrareddy235 <157972541+surendrareddy235@users.noreply.github.com>
2025-02-02 16:05:44 +0530

    Add files via upload

---
+++ hellojava/src/main/java/com/projects/springbootproject6/HellojavaApplication.java
@@ -0,0 +1,13 @@
+package com.projects.springbootproject6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellojavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellojavaApplication.class, args);
+	}
+
+}
